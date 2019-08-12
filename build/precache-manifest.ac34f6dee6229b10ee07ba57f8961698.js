09b2525c05d0cf08a2299c6274c6be28943cb75a
eikcalb <ogbuefi.agwa@gmail.com>
2019-08-12 02:27:43 +0100

    Add files via upload
    Startup Github page for iohub

---
+++ build/precache-manifest.ac34f6dee6229b10ee07ba57f8961698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b87cae8b1b1f160dde7bf52600cce1",
+    "url": "/iohubweb/index.html"
+  },
+  {
+    "revision": "faa3986d4a936be58731",
+    "url": "/iohubweb/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "32496a3c127ee014a936",
+    "url": "/iohubweb/static/css/main.f1ff5ac7.chunk.css"
+  },
+  {
+    "revision": "faa3986d4a936be58731",
+    "url": "/iohubweb/static/js/2.7e5fc70e.chunk.js"
+  },
+  {
+    "revision": "32496a3c127ee014a936",
+    "url": "/iohubweb/static/js/main.edf3fd12.chunk.js"
+  },
+  {
+    "revision": "b67c0e7f20ed4ba010f4",
+    "url": "/iohubweb/static/js/runtime~main.b4068b9a.js"
+  }
+]);
